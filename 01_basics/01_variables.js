c07a04edbefed55599653c471236b5c6fc853499
kunaldevatwal <kunaldevatwal1@gmail.com>
2026-03-31 15:02:36 +0530

    lecture_04

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=101
+let accountEmail="kunaldevatwal@gmail.com"
+var accountpassword="1se8tk"
+accountcity="raipur"
+let accountstate
+//accountId=110 //not valid
+
+accountEmail="kunal@gmail.com"
+
+accountpassword="1se9tk"
+
+accountcity="indore"
+
+console.log(accountcity)
+
+console.table([accountId, accountEmail, accountpassword,accountcity,accountstate])
